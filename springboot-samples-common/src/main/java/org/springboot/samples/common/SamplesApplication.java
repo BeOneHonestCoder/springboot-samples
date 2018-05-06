ace5c0401b4830a21cf9a397b0d15f9e1cfbfb9a
HaiJian <1160033800@qq.com>
2018-05-06 14:23:31 +0800

    add springboot samples

---
+++ springboot-samples-common/src/main/java/org/springboot/samples/common/SamplesApplication.java
@@ -0,0 +1,21 @@
+package org.springboot.samples.common;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@SpringBootApplication
+@EnableDiscoveryClient
+public @interface SamplesApplication {
+
+}
---
+++ springboot-samples-consul/src/main/java/org/springboot/samples/consul/ConsulApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.samples.consul;
+
+import org.springboot.samples.common.SamplesApplication;
+import org.springframework.boot.SpringApplication;
+
+@SamplesApplication
+public class ConsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulApplication.class, args);
+	}
+
+}
