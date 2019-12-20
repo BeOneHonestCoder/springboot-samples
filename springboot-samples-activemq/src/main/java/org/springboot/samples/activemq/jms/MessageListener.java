97e2fc7a8a5329d76500d43f19db8d40e2a10e72
Haijian <1160033800@qq.com>
2019-12-20 17:50:43 +0800

    refactor

--- springboot-samples-activemq/src/main/java/org/springboot/samples/activemq/jms/MessageListener.java
+++ springboot-samples-activemq/src/main/java/org/springboot/samples/activemq/jms/MessageListener.java
@@ -1,17 +1,26 @@
 package org.springboot.samples.activemq.jms;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.stereotype.Component;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.stereotype.Service;
 
-@Component
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+@Service
 public class MessageListener {
 
 	private Logger LOG = Logger.getLogger(getClass());
 
+	@Autowired
+	private MappingJackson2MessageConverter converter;
+
 	@JmsListener(destination = "sample.queue")
-	public void receiveQueue(String text) {
-		LOG.info(text);
+	public void onMessage(Message message) throws JMSException {
+		converter.fromMessage(message);
+		LOG.info(message);
 	}
 
 }
