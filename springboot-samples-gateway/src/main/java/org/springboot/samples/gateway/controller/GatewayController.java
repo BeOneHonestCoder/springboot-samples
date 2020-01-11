4487ea0ed68cf099b79e372f7c28a143646c3948
Haijian <1160033800@qq.com>
2020-01-11 14:46:32 +0800

    add retry

--- springboot-samples-gateway/src/main/java/org/springboot/samples/gateway/controller/GatewayController.java
+++ springboot-samples-gateway/src/main/java/org/springboot/samples/gateway/controller/GatewayController.java
@@ -1,5 +1,10 @@
 package org.springboot.samples.gateway.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +14,29 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/demo")
 public class GatewayController {
 
+	private Logger LOG = LoggerFactory.getLogger(GatewayController.class);
+
 	@GetMapping("/search")
+	@Retryable(value = {ArithmeticException.class}, maxAttempts = 3, backoff = @Backoff(delay = 1000))
 	public Mono<String> sendMessage() throws Exception {
+		LOG.info("Hi");
+		int i = 1/0;
 
 		return Mono.just("hello");
 	}
+
+	@Retryable(value = {ArithmeticException.class}, maxAttempts = 3, backoff = @Backoff(delay = 1000))
+	public void retryMethod() {
+		LOG.info("Hi");
+		int i = 1/0;
+	}
+
+	@Recover
+	public Mono<String> recoverMethod(ArithmeticException exception){
+		LOG.info("recover method");
+		return Mono.just("hello");
+	}
+
+
 
 }
