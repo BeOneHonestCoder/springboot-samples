07821df30744669732abd2dc7cf0f501aac016d6
Haijian <1160033800@qq.com>
2020-05-16 12:24:54 +0800

    add consul

--- springboot-samples-consul/src/main/java/org/springboot/samples/consul/controller/SayHelloController.java
+++ springboot-samples-consul/src/main/java/org/springboot/samples/consul/controller/SayHelloController.java
@@ -1,10 +1,15 @@
 package org.springboot.samples.consul.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SayHelloController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	@RequestMapping("/sayHello")
 	public String sayHello() {
