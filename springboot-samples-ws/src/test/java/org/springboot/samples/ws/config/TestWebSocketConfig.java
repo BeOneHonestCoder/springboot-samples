e0c0c758d2aba762cbe0a40de23e2f8f4fb32680
developer <1160033800@qq.com>
2025-07-21 10:33:15 +0800

    integrate websocket unit case

---
+++ springboot-samples-ws/src/test/java/org/springboot/samples/ws/config/TestWebSocketConfig.java
@@ -0,0 +1,14 @@
+package org.springboot.samples.ws.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestWebSocketConfig {
+
+    @Test
+    public void testStream() {
+        System.out.println("Config");
+    }
+
+}
