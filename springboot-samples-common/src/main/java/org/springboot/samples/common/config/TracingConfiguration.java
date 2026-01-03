33a69d43e1253e8b52e4b8d2de2f76a38c3a9af8
developer <1160033800@qq.com>
2026-01-03 18:03:24 +0800

    integrate OpenTelemetry (OTEL)

--- springboot-samples-common/src/main/java/org/springboot/samples/common/config/TracingConfiguration.java
+++ springboot-samples-common/src/main/java/org/springboot/samples/common/config/TracingConfiguration.java
@@ -1,7 +1,20 @@
 package org.springboot.samples.common.config;
 
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TracingConfiguration {
+
+    @Value("${management.opentelemetry.tracing.export.otlp.endpoint}")
+    private String endpoint;
+
+    @Bean
+    public OtlpHttpSpanExporter otlpHttpSpanExporter() {
+        return OtlpHttpSpanExporter.builder()
+                .setEndpoint(endpoint)
+                .build();
+    }
 }
